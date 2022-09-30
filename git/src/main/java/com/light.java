fee74fc45447b58a2ff435c554de250d80dc4857
light <483452849@qq.com>
2022-09-30 15:29:45 +0800

    first commit light

---
+++ git/src/main/java/com/light.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * @author light
+ * @version 1.0
+ * @project Git
+ * @description
+ * @data 2022/9/30 15 : 26 : 21
+ */
+public class light {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
