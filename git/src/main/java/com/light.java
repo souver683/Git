bb4ed7c6211a3623aa44c5be9f23e1ae3a86db3b
light <483452849@qq.com>
2022-09-30 16:48:06 +0800

    hot-fix commit2

--- git/src/main/java/com/light.java
+++ git/src/main/java/com/light.java
@@ -12,6 +12,8 @@ public class light {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3 hot-fix");
+        System.out.println("hello git3 hot-fix2 test");
+
 
 
 
