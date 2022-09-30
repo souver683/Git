9b891801e9575da9531836e90e91bf52622fdf5c
light <483452849@qq.com>
2022-09-30 17:30:52 +0800

    master commit  ssh

--- git/src/main/java/com/light.java
+++ git/src/main/java/com/light.java
@@ -14,6 +14,7 @@ public class light {
         System.out.println("hello git3 hot-fix");
         System.out.println("hello git3 master 2");
         System.out.println("hello git3 hot-fix2 test");
+        System.out.println("hello git3 hot-fix2 test ssh");
 
 
 
